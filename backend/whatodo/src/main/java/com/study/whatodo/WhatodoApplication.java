2ad884be2fa4a56b9dee35b0d32b4bd7b107df67
abctim <timbella.xyz@gmail.com>
2023-08-10 11:47:03 +0200

    Created a Spring Boot backend

---
+++ backend/whatodo/src/main/java/com/study/whatodo/WhatodoApplication.java
@@ -0,0 +1,13 @@
+package com.study.whatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatodoApplication.class, args);
+	}
+
+}
---
+++ backend/whatodo/src/test/java/com/study/whatodo/WhatodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.whatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
