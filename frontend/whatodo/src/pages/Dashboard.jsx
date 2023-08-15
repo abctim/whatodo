19df9bfaa1627c3e14f889c64e31abf28b3b1672
abctim <timbella.xyz@gmail.com>
2023-08-15 18:38:28 +0200

    Small styling changes

--- frontend/whatodo/src/pages/Dashboard.jsx
+++ frontend/whatodo/src/pages/Dashboard.jsx
@@ -21,6 +21,7 @@ const Dashboard = () => {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
+            marginBottom: 55
           }}
         >
           <InputField />
