878b1db24737f6a6973d9db475adcd038c083224
abctim <timbella.xyz@gmail.com>
2023-08-15 21:56:54 +0200

    Created class TodoService and @Autowired it to TodoEndpoint

--- backend/whatodo/src/main/java/com/study/whatodo/communication/endpoint/TodoEndpoint.java
+++ backend/whatodo/src/main/java/com/study/whatodo/communication/endpoint/TodoEndpoint.java
@@ -1,6 +1,8 @@
 package com.study.whatodo.communication.endpoint;
 
 import com.study.whatodo.persistence.model.Todo;
+import com.study.whatodo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/todos")
 public class TodoEndpoint {
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoEndpoint(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
     @PostMapping("/save")
     public Todo saveTodo(@RequestBody String text) {
       Todo todo = new Todo();
