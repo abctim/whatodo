f8bfd29ec6af9376ef335ea61e43c27003fe3f9f
abctim <timbella.xyz@gmail.com>
2023-08-15 18:56:22 +0200

    Created Todo Entity

---
+++ backend/whatodo/src/main/java/com/study/whatodo/persistence/model/Todo.java
@@ -0,0 +1,42 @@
+package com.study.whatodo.persistence.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    private boolean done = false;
+
+    public Todo() {
+    }
+
+    public Todo(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
