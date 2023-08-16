660ed23b55254cb56b90db9daf6824e046fdd786
abctim <timbella.xyz@gmail.com>
2023-08-16 20:48:16 +0200

    Created proxy todo in TodoList

--- frontend/whatodo/src/components/TodoList.jsx
+++ frontend/whatodo/src/components/TodoList.jsx
@@ -1,53 +1,44 @@
-import React from "react";
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Checkbox from "@mui/material/Checkbox";
+import EditIcon from '@mui/icons-material/Edit';
 
 const TodoList = () => {
+  const [checked, setChecked] = useState(false);
+
+  const handleCheckBoxClick = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <div>
-      <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <Box
+        sx={{
+          width: "50vw",
+          bgcolor: "background.paper",
+        }}
+      >
         <nav aria-label="main mailbox folders">
           <List>
             <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <InboxIcon />
-                </ListItemIcon>
-                <ListItemText primary="Inbox" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <DraftsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Drafts" />
-              </ListItemButton>
+              <Checkbox checked={checked} onChange={handleCheckBoxClick} />
+              <ListItemText primary="This is your first todo" />
+              <IconButton edge="end" aria-label="edit">
+                <EditIcon />
+              </IconButton>
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
             </ListItem>
           </List>
         </nav>
         <Divider />
-        <nav aria-label="secondary mailbox folders">
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText primary="Trash" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton component="a" href="#simple-list">
-                <ListItemText primary="Spam" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </nav>
       </Box>
     </div>
   );
