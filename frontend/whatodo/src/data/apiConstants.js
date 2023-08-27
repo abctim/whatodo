c364bd383980f7e5745e270aec2b7bf7c8d920bb
abctim <timbella.xyz@gmail.com>
2023-08-27 19:36:10 +0200

    Added EDIT_TODO_URL to apiConstants.js

--- frontend/whatodo/src/data/apiConstants.js
+++ frontend/whatodo/src/data/apiConstants.js
@@ -2,3 +2,4 @@
 export const SAVE_TODO_URL = "http://localhost:8080/todos/save";
 export const GET_ALL_TODOS_URL = "http://localhost:8080/todos/get-all";
 export const DELETE_TODO_URL = "http://localhost:8080/todos/delete";
+export const EDIT_TODO_URL = "http://localhost:8080/todos/edit";
